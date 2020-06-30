4c370673fa51a2e316366d158fd360488fe0e24c
Denis Duarte <duarte.denis11@gmail.com>
2020-06-30 13:25:21 -0400

    Added the lombok library as demo. Allows for the use of annotation to reduce boilerplate code of getters and setters.

--- src/org/launchcode/java/studios/restaurant/Menu.java
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -2,6 +2,7 @@ package org.launchcode.java.studios.restaurant;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Menu {
 
@@ -9,7 +10,7 @@ public class Menu {
     private static LocalDate updatedDt;
 
     public Menu(ArrayList<MenuItem> menuItems) {
-        this.menuItems = menuItems;
+        this.menuItems = new ArrayList<>();
         this.setUpdatedDt(LocalDate.now());
     }
 
@@ -27,5 +28,45 @@ public class Menu {
 
     public void setUpdatedDt(LocalDate updatedDt) {
         Menu.updatedDt = updatedDt;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        ArrayList<MenuItem> items = this.getMenuItems();
+        items.add(menuItem);
+        this.setMenuItems(items);
+    }
+
+    public void addMenuItem(List<MenuItem> menuItems) {
+        ArrayList<MenuItem> items = this.getMenuItems();
+        items.addAll(menuItems);
+        this.setMenuItems(items);
+    }
+
+    public void removeMenuItem(MenuItem menuItem) {
+        ArrayList<MenuItem> items = this.getMenuItems();
+        items.remove(menuItem);
+        this.setMenuItems(items);
+    }
+
+    public void removeMenuItem(List<MenuItem> menuItems) {
+        ArrayList<MenuItem> items = this.getMenuItems();
+        items.removeAll(menuItems);
+        this.setMenuItems(items);
+    }
+
+    public void showSingleMenuItem(MenuItem menuItem) {
+        System.out.println(menuItem.toString());
+    }
+
+    public void showAllMenuItems(List<MenuItem> menuItems) {
+        for (MenuItem m : menuItems) {
+            System.out.println(m.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        MenuItem menuItem = new MenuItem(4, "Hot Dog", "Main Course");
+        System.out.println(menuItem);
+        System.out.println(menuItem.getPrice());
     }
 }
--- src/org/launchcode/java/studios/restaurant/MenuItem.java
+++ src/org/launchcode/java/studios/restaurant/MenuItem.java
@@ -1,67 +1,39 @@
 package org.launchcode.java.studios.restaurant;
 
-import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+@Getter
+@Setter
 public class MenuItem {
 
     enum Category {
-        Appetizer, Main_Course, Dessert
+        Appetizer, Main_Course, Dessert;
     }
 
     private double price;
     private String description;
     private MenuItem.Category category;
     private final LocalDate createdDt;
-    private boolean isNew;
+    private @Setter(AccessLevel.NONE) boolean isNew;
 
     public MenuItem(double price, String description, Category category) {
-        this.price = price;
+        this.price =  price;
         this.description = description;
         this.category = category;
         this.createdDt = LocalDate.now();
     }
 
     public MenuItem(double price, String description, String category) {
-        this.price = price;
+        this.price =  price;
         this.description = description;
         this.category = translateCategory(category);
         this.createdDt = LocalDate.now();
-    }
-
-    public boolean isNew() {
-        boolean aNew = false;
-        if (LocalDate.now().getDayOfYear() - this.createdDt.getDayOfYear() <= 5) {
-            aNew = true;
-        }
-        return aNew;
-    }
-
-    public LocalDate getCreatedDt() {
-        return createdDt;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
     }
 
     private Category translateCategory(String category) {
@@ -74,4 +46,42 @@ public class MenuItem {
         return cat;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        if (this.category.equals(Category.Appetizer)) {
+            str.append("Appetizer");
+        } else if (this.category.equals(Category.Main_Course)) {
+            str.append("Main Course");
+        } else {
+            str.append("Dessert");
+        }
+        str.append("\n****************");
+        str.append("\n");
+        str.append("Description: ");
+        str.append(this.description);
+        str.append("\n");
+        str.append("Price: $");
+        str.append(this.price);
+        if (String.valueOf(this.price).substring(String.valueOf(this.price).indexOf(".")).length() == 2) {
+            str.append("0");
+        }
+        str.append("\n");
+        return str.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Double.compare(menuItem.price, price) == 0 &&
+                description.equals(menuItem.description) &&
+                category == menuItem.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, description, category);
+    }
 }
